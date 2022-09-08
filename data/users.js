c46dd15427b1711d65166288879ac1e86972b402
Steven Johnson <steven.johnson@madgex.com>
2022-09-08 13:49:35 +0100

    styling stuff

--- data/users.js
+++ data/users.js
@@ -3,45 +3,28 @@ const users = [
         id: "0",
         name: "Steven Johnson",
         email: "steven.johnson@madgex.com",
-        feedback: [
-            {
-                id: "142353",
-                category: "Test",
-                description: "He is the best!",
-                likes: "0",
-            },
-            {
-                id: "14235dfgdf3",
-                category: "Test",
-                description: "He is way better than the rest!",
-                likes: "0",
-            },
-        ],
+        image: "/public/images/steven.png",
+        feedback: [],
     },
     {
         id: "1",
         name: "Nathan Buckley",
         email: "nathan.buckley@madgex.com",
-        feedback: [
-            {
-                id: "11234234",
-                category: "Test",
-                description:
-                    "Thank you for putting in the extra effort during this busy time at work. You are a valuable member of our team, and your positive attitude has helped us all continue to feel motivated.",
-                likes: "14",
-            },
-        ],
+        image: "/public/images/nathan.png",
+        feedback: [],
     },
     {
         id: "2",
         name: "Lisa Takken",
         email: "lisa.takken@madgex.com",
+        image: "/public/images/lisa.png",
         feedback: [],
     },
     {
         id: "3",
         name: "Max Keetley",
         email: "max.keetley@madgex.com",
+        image: "/public/images/max.png",
         feedback: [],
     },
 ];
