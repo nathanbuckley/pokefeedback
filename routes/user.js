07372d97964aad7e92ad0027ac6ba15c7acb1b1c
Steven Johnson <steven.johnson@madgex.com>
2022-09-08 10:59:03 +0100

    Actually working now

--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,22 @@ const path = require("path");
 const feedbackData = require("../data/users");
 
 module.exports = [
+    // {
+    //     method: "GET",
+    //     path: "/{userId}",
+    //     handler: (request, h) => {
+    //         return h.view("user", { feedbackData });
+    //     },
+    // },
     {
         method: "GET",
         path: "/{userId}",
         handler: (request, h) => {
             const userId = request.params.userId;
-            // if project not in projectData then throw boom.notfound (checkout hapi boom)
-            // else
+            console.log(userId);
+
             const feedback = feedbackData.filter((item) => item.id === userId);
-            return h.view("user", { feedback: feedback[0].feedback });
+            return h.view("user", { user: feedback[0] });
         },
     },
 ];
