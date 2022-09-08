a807a54bf5b49d780f41e39cf464975293762ff3
Steven Johnson <steven.johnson@madgex.com>
2022-09-08 10:34:18 +0100

    Added some stuff and dev run script

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+const feedbackData = require("../data/users");
+
+module.exports = [
+    {
+        method: "GET",
+        path: "/{userId}",
+        handler: (request, h) => {
+            const userId = request.params.userId;
+            // if project not in projectData then throw boom.notfound (checkout hapi boom)
+            // else
+            const feedback = feedbackData.filter((item) => item.id === userId);
+            return h.view("user", { feedback: feedback[0] });
+        },
+    },
+];
